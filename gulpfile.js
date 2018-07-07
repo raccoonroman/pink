bf85eec7cb35ba735a0ad892bbc25662d36628b7
Роман <kromanjs@gmail.com>
2018-07-07 22:54:26 +0300

    Connect fonts, added general styles

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,11 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
  
 gulp.task('sass', function () {
-	return gulp.src('./source/sass/*.scss')
+	return gulp.src('./source/sass/style.scss')
 		.pipe(sass().on('error', sass.logError))
 		.pipe(gulp.dest('./source/sass'));
 });
  
 gulp.task('sass:watch', function () {
-	gulp.watch('./source/sass/*.scss', ['sass']);
+	gulp.watch('./source/sass/style.scss', ['sass']);
 });
