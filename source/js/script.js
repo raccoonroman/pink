308f9ca925c42e04231372d7234821df4dc0804f
Роман <kromanjs@gmail.com>
2018-07-15 13:34:51 +0300

    connected price slider

--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,16 @@
-$('.reviews-slider__list').slick({
+$('.slider__list--reviews').slick({
 	dots: true,
 	arrows: false,
 	adaptiveHeight: true,
-	appendDots: ".reviews-slider__dots",
-	dotsClass: "reviews-slider__dots-list"
+	appendDots: ".slider__dots--reviews",
+	dotsClass: "dots-list"
+});
+
+$('.slider__list--price').slick({
+	dots: true,
+	arrows: false,
+	centerMode: true,
+	centerPadding: "21px",
+	appendDots: ".slider__dots--price",
+	dotsClass: "dots-list"
 });
