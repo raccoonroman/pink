9de92a0a005f9bae479d97e6d54a171652aa2f57
Роман <kromanjs@gmail.com>
2018-08-11 11:23:49 +0300

    experiments with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
 'use strict';
- 
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
- 
+
 gulp.task('sass', function () {
-	return gulp.src('./source/sass/style.scss')
-		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest('./source/sass'));
+  return gulp.src('source/sass/style.scss')
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(gulp.dest('source/css'));
 });
- 
+
 gulp.task('sass:watch', function () {
-	gulp.watch('./source/sass/style.scss', ['sass']);
+  gulp.watch('source/sass/style.scss', ['sass']);
 });
